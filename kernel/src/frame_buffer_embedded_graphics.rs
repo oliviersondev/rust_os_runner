f873f965aa38c171a08c5141c6273064b1e08eb9
olivier sonrel <ext_olivier.sonrel@ras-interim.fr>
2026-03-18 09:20:40 +0100

    todo integrate FrameBufferEmbeddedGraphics into main

---
+++ kernel/src/frame_buffer_embedded_graphics.rs
@@ -0,0 +1,71 @@
+use core::convert::Infallible;
+use crate::FrameBufferInfo;
+use core::num::NonZero;
+use core::ptr::{slice_from_raw_parts_mut, NonNull};
+use embedded_graphics::Pixel;
+use embedded_graphics::pixelcolor::Rgb888;
+use embedded_graphics::prelude::{Dimensions, DrawTarget, Point, Size};
+use embedded_graphics::primitives::Rectangle;
+
+pub struct FrameBufferEmbeddedGraphics<'a> {
+    buffer: &'a mut [u32],
+    info: FrameBufferInfo,
+    pixel_pitch: usize,
+    bounding_box: Rectangle,
+}
+
+impl FrameBufferEmbeddedGraphics<'_> {
+    /// # Safety
+    /// The frame buffer must be mapped at `addr`
+    pub unsafe fn new(addr: NonZero<usize>, info: FrameBufferInfo) -> Self {
+        if info.bits_per_pixel as u32 == u32::BITS {
+            Self {
+                buffer: {
+                    let mut ptr = NonNull::new(slice_from_raw_parts_mut(
+                        addr.get() as *mut u32,
+                        (info.pitch * info.height) as usize / size_of::<u32>(),
+                    )).unwrap();
+                    // Safety : this memory mapped
+                    unsafe { ptr.as_mut() }
+                },
+                info,
+                pixel_pitch: info.pitch as usize / size_of::<u32>(),
+                bounding_box: Rectangle {
+                    top_left: Point::zero(),
+                    size: Size {
+                        width: info.width.try_into().unwrap(),
+                        height: info.height.try_into().unwrap(),
+                    },
+                },
+            }
+        } else {
+            panic!("DrawTarget implemented for RGB888, but bpp doesn't match RGB888");
+        }
+    }
+}
+
+impl Dimensions for FrameBufferEmbeddedGraphics<'_> {
+    fn bounding_box(&self) -> Rectangle {
+        self.bounding_box
+    }
+}
+
+impl DrawTarget for FrameBufferEmbeddedGraphics<'_> {
+    type Color = Rgb888;
+    type Error = Infallible;
+
+    fn draw_iter<I>(&mut self, pixels: I) -> Result<(), Self::Error>
+    where
+        I: IntoIterator<Item = Pixel<Self::Color>>,
+    {
+        let bounding_box = self.bounding_box;
+        pixels
+            .into_iter()
+            .filter(|Pixel(point, _)| bounding_box.contains(*point))
+            .for_each(|Pixel(point, color)| {
+                let pixel_index = point.y as usize * self.pixel_pitch + point.x as usize;
+                self.buffer[pixel_index] = self.info.pixel_info.build_pixel(&color);
+            });
+        Ok(())
+    }
+}
--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,5 +1,12 @@
 #![no_std]
 
+mod frame_buffer_info;
+mod rgb_pixel_info;
+mod frame_buffer_embedded_graphics;
+
+pub use rgb_pixel_info::RgbPixelInfo;
+pub use frame_buffer_info::FrameBufferInfo;
+
 pub fn hlt_loop() -> ! {
     loop {
         x86_64::instructions::hlt();
